703c6e7803c7f5ef2c4807f80714a53710b20d52
Ana Beatriz Barbosa Yoshida <ra245609@rabugento.lab.ic.unicamp.br>
2025-05-28 14:56:30 -0300

    respostas respondidas

--- lab07/inner_cache.sv
+++ lab07/inner_cache.sv
@@ -1,3 +1,17 @@
+
+/*
+- Como você calcula o tamanho em bytes da capacidade sua cache?
+A capacidade útil da cache é calculada pelo número de linhas multiplicado pelo tamanho dos dados em cada linha, 
+nesse caso temos 16 linhas e cada pode armazenar 32 bits(4 bytes),portanto, a capacidade útil é de 64 bytes.
+
+- Qual o tamanho total da sua cache em bytes?  
+Cada linha tem 1 bit (valid), 10 bits (tag) e 32 bits (data) = 43 bits por linha, portanto, o tamanho total será dado por:
+16 linhasX43 bits = 688 bits/8 = 86 bytes
+
+Qual o aproveitamento de espaço da sua cache (capacidade / espaço total)?
+64 bytes úteis/86 bytes totais = 74.4%
+*/
+
 module inner_cache #(
   parameter DATA_WIDTH   = 32,
   parameter ADDR_WIDTH   = 16,
